229fb8de2e2cc23759df18444e903d602c2f1922
Usuario <usuario@Usuarios-iMac.local>
2022-11-22 12:48:12 -0300

    analisis

---
+++ proc/analisis-datos.R
@@ -0,0 +1,44 @@
+# Análisis del trabajo de cuidados estudiantes de FACSO 2022
+# Moira Martínez, Catalina Navia y Ambar Santander
+# Curso Encuestas Sociales, Noviembre 2022
+
+#----1. LIBRERÍAS Y BASE DE DATOS ----
+
+load('proc/encuesta_cuidados.RData') # El nombre de la base es proc_encuesta
+
+summary(proc_encuesta) # Revisión general
+
+names(proc_encuesta)
+
+# Variables:
+# Session
+# si_no_cuidado
+# cuidado_tiempo
+# cuidado_comer
+# cuidado_acostar
+# cuidado_mudar
+# cuidado_asear
+# cuidado_vestir
+# cuidado_aconsejar
+# cuidado_salud
+# cuidado_acompanar1
+# cuidado_acompanar2
+# cuidado_tareas
+# cuidado_jugar
+# cuidado_leer
+# cuidado_multiple_1
+# cuidado_multiple_2
+# cuidado_multiple_3
+# satisfaccion_vida
+# miembros_hogar
+# miembros_cuidado
+# hijos
+# cantidad_hijos
+# vive_padres
+# e_civil
+# edad
+# carrera
+# id_genero
+# id_genero_otra
+
+
